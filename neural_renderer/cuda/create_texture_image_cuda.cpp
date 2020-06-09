e0b20132cf2fec952e33be0698a1fc04e023bc9f
lstcutong <42967350+lstcutong@users.noreply.github.com>
2020-06-09 14:43:16 +0800

    Update create_texture_image_cuda.cpp

--- neural_renderer/cuda/create_texture_image_cuda.cpp
+++ neural_renderer/cuda/create_texture_image_cuda.cpp
@@ -10,9 +10,9 @@ at::Tensor create_texture_image_cuda(
 
 // C++ interface
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+//#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+//#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
+//#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 
 at::Tensor create_texture_image(
