eb629d370ec230e7537e68455c6e1e812e09ef9a
lstcutong <42967350+lstcutong@users.noreply.github.com>
2020-06-09 14:46:09 +0800

    Update rasterize_cuda.cpp

--- neural_renderer/cuda/rasterize_cuda.cpp
+++ neural_renderer/cuda/rasterize_cuda.cpp
@@ -63,9 +63,9 @@ at::Tensor backward_depth_map_cuda(
 
 // C++ interface
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+//#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+//#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
+//#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 std::vector<at::Tensor> forward_face_index_map(
         at::Tensor faces,
@@ -81,12 +81,12 @@ std::vector<at::Tensor> forward_face_index_map(
         int return_alpha,
         int return_depth) {
 
-    CHECK_INPUT(faces);
-    CHECK_INPUT(face_index_map);
-    CHECK_INPUT(weight_map);
-    CHECK_INPUT(depth_map);
-    CHECK_INPUT(face_inv_map);
-    CHECK_INPUT(faces_inv);
+    //CHECK_INPUT(faces);
+    //CHECK_INPUT(face_index_map);
+    //CHECK_INPUT(weight_map);
+    //CHECK_INPUT(depth_map);
+    //CHECK_INPUT(face_inv_map);
+    //CHECK_INPUT(faces_inv);
 
     return forward_face_index_map_cuda(faces, face_index_map, weight_map,
                                        depth_map, face_inv_map, faces_inv,
@@ -106,14 +106,14 @@ std::vector<at::Tensor> forward_texture_sampling(
         int image_size,
         float eps) {
 
-    CHECK_INPUT(faces);
-    CHECK_INPUT(textures);
-    CHECK_INPUT(face_index_map);
-    CHECK_INPUT(weight_map);
-    CHECK_INPUT(depth_map);
-    CHECK_INPUT(rgb_map);
-    CHECK_INPUT(sampling_index_map);
-    CHECK_INPUT(sampling_weight_map);
+    //CHECK_INPUT(faces);
+    //CHECK_INPUT(textures);
+    //CHECK_INPUT(face_index_map);
+    //CHECK_INPUT(weight_map);
+    //CHECK_INPUT(depth_map);
+    //CHECK_INPUT(rgb_map);
+    //CHECK_INPUT(sampling_index_map);
+    //CHECK_INPUT(sampling_weight_map);
 
     return forward_texture_sampling_cuda(faces, textures, face_index_map,
                                     weight_map, depth_map, rgb_map,
@@ -134,13 +134,13 @@ at::Tensor backward_pixel_map(
         int return_rgb,
         int return_alpha) {
 
-    CHECK_INPUT(faces);
-    CHECK_INPUT(face_index_map);
-    CHECK_INPUT(rgb_map);
-    CHECK_INPUT(alpha_map);
-    CHECK_INPUT(grad_rgb_map);
-    CHECK_INPUT(grad_alpha_map);
-    CHECK_INPUT(grad_faces);
+    //CHECK_INPUT(faces);
+    //CHECK_INPUT(face_index_map);
+    //CHECK_INPUT(rgb_map);
+    //CHECK_INPUT(alpha_map);
+    //CHECK_INPUT(grad_rgb_map);
+    //CHECK_INPUT(grad_alpha_map);
+    //CHECK_INPUT(grad_faces);
 
     return backward_pixel_map_cuda(faces, face_index_map, rgb_map, alpha_map,
                                    grad_rgb_map, grad_alpha_map, grad_faces,
@@ -155,11 +155,11 @@ at::Tensor backward_textures(
         at::Tensor grad_textures,
         int num_faces) {
 
-    CHECK_INPUT(face_index_map);
-    CHECK_INPUT(sampling_weight_map);
-    CHECK_INPUT(sampling_index_map);
-    CHECK_INPUT(grad_rgb_map);
-    CHECK_INPUT(grad_textures);
+    //CHECK_INPUT(face_index_map);
+    //CHECK_INPUT(sampling_weight_map);
+    //CHECK_INPUT(sampling_index_map);
+    //CHECK_INPUT(grad_rgb_map);
+    //CHECK_INPUT(grad_textures);
 
     return backward_textures_cuda(face_index_map, sampling_weight_map,
                                   sampling_index_map, grad_rgb_map,
@@ -176,13 +176,13 @@ at::Tensor backward_depth_map(
         at::Tensor grad_faces,
         int image_size) {
 
-    CHECK_INPUT(faces);
-    CHECK_INPUT(depth_map);
-    CHECK_INPUT(face_index_map);
-    CHECK_INPUT(face_inv_map);
-    CHECK_INPUT(weight_map);
-    CHECK_INPUT(grad_depth_map);
-    CHECK_INPUT(grad_faces);
+    //CHECK_INPUT(faces);
+    //CHECK_INPUT(depth_map);
+    //CHECK_INPUT(face_index_map);
+    //CHECK_INPUT(face_inv_map);
+    //CHECK_INPUT(weight_map);
+    //CHECK_INPUT(grad_depth_map);
+    //CHECK_INPUT(grad_faces);
 
     return backward_depth_map_cuda(faces, depth_map, face_index_map,
                                    face_inv_map, weight_map,
